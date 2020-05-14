0bacd1a5036eb1e6a62d10304ac6b1a6dbc217a5
tanku3080 <57029371+tanku3080@users.noreply.github.com>
2020-05-14 15:00:26 +0900

    セーブ機能スクリプト
    playerPrefsを使ったスクリプトとJsonを使うスクリプトの二種類が入っている

---
+++ PlayerPrefs2/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;   // Serializable を指定するために必要
+
+/// <summary>
+/// アプリの設定情報を格納するクラス
+/// </summary>
+[Serializable]  // JsonUtility を使うために必要
+public class Configuration  // シリアライズするクラスは MonoBehaviour を継承してはいけない
+{
+    public float Volume;
+    public int GraphicsQuality;
+    public bool Is60Fps;
+    public bool IsLeftHanded;
+}
